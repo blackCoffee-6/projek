1180e412be2a0d5ff707c2a967529603598260a5
zahara120 <zaharaa120@google.com>
2021-05-25 21:23:19 +0700

    benerin error val_b

--- app/Http/Controllers/KajianController.php
+++ app/Http/Controllers/KajianController.php
@@ -179,66 +179,46 @@ class KajianController extends Controller
     public function update(Request $request, $kajian_id)
     {
         // dd($request->all());
-        Kajian::findOrFail($kajian_id)->update([
-            'ket_up' => implode(',', $request->ket_up),
-            'ch_dis' => implode(',', $request->ch_dis),
+        $kajians = Kajian::findOrFail($kajian_id);
+        
+        if($request->ru_b != null){
+            $request->request->add(['ru_b' => implode(',', $request->ru_b)]);
+        }
+        else{
+            $request->request->add(['ru_b' => ""]);
+        }
 
-            'ru_a' => $request->ru_a,
-            'ru_b' => implode(',', $request->ru_b),
-            'ru_ket' => $request->ru_ket,
+        if($request->st_b != null){
+            $request->request->add(['st_b' => implode(',', $request->st_b)]);
+        }
+        else{
+            $request->request->add(['st_b' => ""]);
+        }
 
-            'ri_a' => $request->ri_a,
-            'ri_b' => $request->ri_b,
-            'ri_ket' => $request->ri_ket,
+        if($request->val_b != null){
+            $request->request->add(['val_b' => implode(',', $request->val_b)]);
+        }
+        else{
+            $request->request->add(['val_b' => ""]);
+        }
 
-            'st_a' => $request->st_a,
-            'st_b' => implode(',', $request->st_b),
-            'st_ket' => $request->st_ket,
+        if($request->tr_b != null){
+            $request->request->add(['tr_b' => implode(',', $request->tr_b)]);
+        }
+        else{
+            $request->request->add(['tr_b' => ""]);
+        }
 
-            'me_a' => $request->me_a,
-            'me_ket' => $request->me_ket,
+        if($request->ket_up != null){
+            $request->request->add(['ket_up' => implode(',', $request->ket_up)]);
+        }
+        
+        if($request->ch_dis != null){
+            $request->request->add(['ch_dis' => implode(',', $request->ch_dis)]);
+        }
 
-            'val_a' => $request->val_a,
-            'val_b' => implode(',', $request->val_b),
-            'val_ket' => $request->val_ket,
-
-            'tr_a' => $request->tr_a,
-            'tr_b' => implode(',', $request->tr_b),
-            'tr_ket' => $request->tr_ket,
-
-            'pr_a' => $request->pr_a,
-            'pr_dok' => $request->pr_dok,
-            'pr_ket' => $request->pr_ket,
-
-            'dok_a' => $request->dok_a,
-            'dok_b' => $request->dok_b,
-            'dok_ket' => $request->dok_ket,
-
-            'si_a' => $request->si_a,
-            'si_ket' => $request->si_ket,
-
-            'kj_tambahan' => $request->kj_tambahan,
-
-            'severity1' => $request->severity,
-            'detec1' => $request->detec,
-            'occur1' => $request->occur,
-
-            'getsev' => $request->getsev,
-            'getdet' => $request->getdet,
-            'getocc' => $request->getocc,
-            'result_dxo' => $request->result_dxo,
-
-            'ch_kategori' => $request->ch_kategori,
-            'ch_status' => $request->ch_status,
-
-            'asman_nama' => $request->asman_nama,
-            'asman_komentar' => $request->asman_komentar,
-            'asman_date' => $request->asman_date,
-
-            'aq_nama' => $request->aq_nama,
-            'aq_komentar' => $request->aq_komentar,
-            'aq_date' => $request->aq_date
-        ]);
+        $input = $request->all();
+        $kajians->fill($input)->save();
 
         return redirect('/List/Kajian')->with('alert', "Kajian Updated Successfully!");
     }
