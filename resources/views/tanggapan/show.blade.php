3cf11cee2184e43ef5622e27ae7a9e05d7ec57dc
blackCoffee-6 <anisahblue@gmail.com>
2021-05-31 19:27:45 +0700

    udh bisa nambah tanggapan tpi remove_btn nya blm bisa

--- resources/views/tanggapan/show.blade.php
+++ resources/views/tanggapan/show.blade.php
@@ -81,23 +81,25 @@
         </div>
     
     <div class="container">
-        <button class="btn btn-success my-2  mx-3">Submit</button>
+        <button class="btn btn-success my-2 mx-3">Submit</button>
         <a href="/home" class="btn btn-danger my-3">Cancel</a>
         </form>
     </div>
 
 <script type="text/javascript">
     $(function(){
-        var count=0;
-        $('#add_btn').click(function(e){
-            e.preventDefault();
-            $("#dynamic_field_append").append('<div id="dynamic_field_tanggapan"><table class="table table-bordered"><thead><tr><th>Tanggapan dari Bidang :@if(Auth::user()->role == "Admin")<br><br><select name="tg_bidangs[]"><option>Select Bidang</option>@foreach($bidangs as $bidang)<option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>@endforeach</select>@endif</th></tr></thead><tbody><tr><td><textarea class="form-control" rows="3" name="tg_bidang[]"></textarea></td></tr></tbody></table><div class="container text-right"><a class="btn btn-danger dynamic_field_remove" id="dynamic_field_remove"><i class="fa fa-close fa-fw"></i></a></div></div><div id="dynamic_field_nama><table class="table table-bordered"><thead><tr><th scope="col">Nama :</th><th scope="col">Tanggal :</th></tr></thead><tbody><tr><td><input class="form-control" type="text" name="tg_nama[]"></td><td><input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]"></td></tr></tbody></table></div></div>');
-            $(".dynamic_field_focus").last().focus();
+        $(document).ready(function(){
+            $('#add_btn').on('click', function(){
+                var html='';
+                html+='<div id="dynamic_field_tanggapan"><table class="table table-bordered my-3"><thead><tr><th>Tanggapan dari Bidang : @if(Auth::user()->role == "Admin")<br></br><select name="tg_bidang[]"><option>Select Bidang</option>@foreach($bidangs as $bidang)<option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>@endforeach</select>@endif</th></tr></thead><tbody><tr><td><textarea class="form-control" rows="3" name="tg_bidang[]"></textarea></td></tr></tbody></table></div><div id="dynamic_field_nama"><table class="table table-bordered"><thead><tr><th scope="col">Nama :</th><th scope="col">Tanggal (Bulan/Tanggal/Tahun) :</th></tr><tbody><tr><td><input class="form-control" type="text" name="tg_nama[]"></td><td><input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]"></td></tr></tbody></table></div><div class="container mt-3 text-right"><button type="button" class="btn btn-danger mx-2" id="remove_btn"><i class="fa fa-minus"></i></button></div>';
+                $('#dynamic_field_append').append(html);
+            })
         });
-        $(document).on('click', '#dynamic_field_remove', function (e) {
-            e.preventDefault();
-            $(this).closest('.dynamic_field_tanggapan').remove();
-        });
+    });
+
+    $(document).on('click', '#remove_btn', function(){
+        // alert();
+        $(this).closest('table').remove();
     });
 </script>
 @endsection
