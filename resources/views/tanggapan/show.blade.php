066ab667906b1f948618cf6579428e64e5f25ec6
blackCoffee-6 <anisahblue@gmail.com>
2021-05-31 15:42:24 +0700

    nyoba tambah tanggapan

--- resources/views/tanggapan/show.blade.php
+++ resources/views/tanggapan/show.blade.php
@@ -27,7 +27,8 @@
     </div>
     <form action="/Store/Tanggapan/{{$fup->id}}" method="POST">
     @csrf
-    <div class="container my-4">
+    <div class="container my-4" id="dynamic_field_append">
+        <div id="dynamic_field_tanggapan">
         <table class="table table-bordered my-3">
             <thead>
                 <tr>
@@ -53,6 +54,8 @@
                 </tr>
             </tbody>
         </table>
+        </div>
+        <div id="dynamic_field_nama">
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -71,32 +74,30 @@
                 </tr>
             </tbody>
         </table>
-        <div class="container text-right">
+        </div>
+    </div>
+        <div class="container text-right mx-2">
             <button type="button" class="btn btn-success" id="add_btn"><i class="fa fa-plus"></i></button>
         </div>
-        
-        <button class="btn btn-success my-2  mx-2">Submit</button>
-        <a href="/home" class="btn btn-danger my-2">Cancel</a>
+    
+    <div class="container">
+        <button class="btn btn-success my-2  mx-3">Submit</button>
+        <a href="/home" class="btn btn-danger my-3">Cancel</a>
         </form>
     </div>
 
 <script type="text/javascript">
-    $(document).ready(function(){
-        $('#add_btn').on('click', function(){
-            var html='';
-            html+='<tr>';
-            html+='<td><textarea class="form-control" rows="3" name="tg_bidang[]"></textarea></td>';
-            html+='<td><input class="form-control" type="text" name="tg_nama[]"></td>';
-            html+='<td><input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]"></td>';
-            html+='</tr>';
-            html+='<div class="container text-right"><button type="button" class="btn btn-danger" id="remove_btn"><i class="fa fa-plus"></i></button></div>';
-            $('tbody').append(html);
-        })
-    });
-
-    $(document).on('click', '#remove_btn', function(){
-        $(this).closest('tr').remove();
+    $(function(){
+        var count=0;
+        $('#add_btn').click(function(e){
+            e.preventDefault();
+            $("#dynamic_field_append").append('<div id="dynamic_field_tanggapan"><table class="table table-bordered"><thead><tr><th>Tanggapan dari Bidang :@if(Auth::user()->role == "Admin")<br><br><select name="tg_bidangs[]"><option>Select Bidang</option>@foreach($bidangs as $bidang)<option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>@endforeach</select>@endif</th></tr></thead><tbody><tr><td><textarea class="form-control" rows="3" name="tg_bidang[]"></textarea></td></tr></tbody></table><div class="container text-right"><a class="btn btn-danger dynamic_field_remove" id="dynamic_field_remove"><i class="fa fa-close fa-fw"></i></a></div></div><div id="dynamic_field_nama><table class="table table-bordered"><thead><tr><th scope="col">Nama :</th><th scope="col">Tanggal :</th></tr></thead><tbody><tr><td><input class="form-control" type="text" name="tg_nama[]"></td><td><input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]"></td></tr></tbody></table></div></div>');
+            $(".dynamic_field_focus").last().focus();
+        });
+        $(document).on('click', '#dynamic_field_remove', function (e) {
+            e.preventDefault();
+            $(this).closest('.dynamic_field_tanggapan').remove();
+        });
     });
 </script>
-</div>
 @endsection
