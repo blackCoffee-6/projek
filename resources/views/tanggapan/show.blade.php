302ce809c54b77f94614b4c81cc4679189984f35
zahara120 <zaharaa120@google.com>
2021-06-02 15:46:00 +0700

    tanggapan dynamically field DONE

--- resources/views/tanggapan/show.blade.php
+++ resources/views/tanggapan/show.blade.php
@@ -27,79 +27,71 @@
     </div>
     <form action="/Store/Tanggapan/{{$fup->id}}" method="POST">
     @csrf
-    <div class="container my-4" id="dynamic_field_append">
-        <div id="dynamic_field_tanggapan">
-        <table class="table table-bordered my-3">
-            <thead>
-                <tr>
-                    <th>
-                        A. Tanggapan dari Bidang :
-                        @if(Auth::user()->role == 'Admin')
-                            <br><br>
-                            <select name="tg_bidangs[]">
-                                <option>Select Bidang</option>
-                                @foreach($bidangs as $bidang)
-                                <option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>
-                                @endforeach
-                            </select>
-                        @endif
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <textarea class="form-control" rows="3" name="tg_bidang[]"></textarea>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="container my-4, row" id="dynamic_field_append">
+            <table class="table table-bordered my-3">
+                <thead>
+                    <tr>
+                        <th>
+                            A. Tanggapan dari Bidang :
+                            @if(Auth::user()->role == 'Admin')
+                                <br><br>
+                                <select name="tg_bidangs[]">
+                                    <option>Select Bidang</option>
+                                    @foreach($bidangs as $bidang)
+                                    <option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>
+                                    @endforeach
+                                </select>
+                            @endif
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <textarea class="form-control" rows="3" name="tg_bidang[]"></textarea>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">Nama :</th>
+                        <th scope="col">Tanggal (Bulan/Tanggal/Tahun) :</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <input class="form-control" type="text" name="tg_nama[]">
+                        </td>
+                        <td>
+                            <input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]">
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        <div id="dynamic_field_nama">
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Nama :</th>
-                    <th scope="col">Tanggal (Bulan/Tanggal/Tahun) :</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <input class="form-control" type="text" name="tg_nama[]">
-                    </td>
-                    <td>
-                        <input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+            <div id="newRow"></div>
+            <button type="button" class="btn btn-success mt-2" id="add_btn"><i class="fa fa-plus"></i> Add more</button>
+        
+        <div class="container">
+            <button class="btn btn-success my-2 mx-3">Submit</button>
+            <a href="/home" class="btn btn-danger my-3">Cancel</a>
+            </form>
         </div>
-    </div>
-        <div class="container text-right mx-2">
-            <button type="button" class="btn btn-success" id="add_btn"><i class="fa fa-plus"></i></button>
-        </div>
-    
-    <div class="container">
-        <button class="btn btn-success my-2 mx-3">Submit</button>
-        <a href="/home" class="btn btn-danger my-3">Cancel</a>
-        </form>
-    </div>
+<script>
+    // add table
+    $("#add_btn").click(function () {
+        var html = '';
+        html+='<div id="dynamic_field_append"><table class="table table-bordered my-3"><thead><tr><th>A. Tanggapan dari Bidang :@if(Auth::user()->role == 'Admin')<br><br><select name="tg_bidangs[]"><option>Select Bidang</option>@foreach($bidangs as $bidang)<option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>@endforeach</select>@endif</th></tr></thead><tbody><tr><td><textarea class="form-control" rows="3" name="tg_bidang[]"></textarea></td></tr></tbody></table><table class="table table-bordered"><thead><tr><th scope="col">Nama :</th><th scope="col">Tanggal (Bulan/Tanggal/Tahun) :</th></tr></thead><tbody><tr><td><input class="form-control" type="text" name="tg_nama[]"></td><td><input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]"></td></tr></tbody></table><button type="button" class="btn btn-danger" id="remove_btn"><i class="fa fa-minus"></i>  Delete</button></div><div class="container text-right mx-2"></div>';
 
-<script type="text/javascript">
-    $(function(){
-        $(document).ready(function(){
-            $('#add_btn').on('click', function(){
-                var html='';
-                html+='<div id="dynamic_field_tanggapan"><table class="table table-bordered my-3"><thead><tr><th>Tanggapan dari Bidang : @if(Auth::user()->role == "Admin")<br></br><select name="tg_bidang[]"><option>Select Bidang</option>@foreach($bidangs as $bidang)<option value="{{$bidang->Bidang->name}}">{{$bidang->Bidang->name}}</option>@endforeach</select>@endif</th></tr></thead><tbody><tr><td><textarea class="form-control" rows="3" name="tg_bidang[]"></textarea></td></tr></tbody></table></div><div id="dynamic_field_nama"><table class="table table-bordered"><thead><tr><th scope="col">Nama :</th><th scope="col">Tanggal (Bulan/Tanggal/Tahun) :</th></tr><tbody><tr><td><input class="form-control" type="text" name="tg_nama[]"></td><td><input class="form-control" type="date" value="{{ date('Y-m-d') }}" name="tg_date[]"></td></tr></tbody></table></div><div class="container mt-3 text-right"><button type="button" class="btn btn-danger mx-2" id="remove_btn"><i class="fa fa-minus"></i></button></div>';
-                $('#dynamic_field_append').append(html);
-            })
-        });
+        $('#newRow').append(html);
     });
 
-    $(document).on('click', '#remove_btn', function(){
-        // alert();
-        $(this).closest('table').remove();
+    // remove table
+    $(document).on('click', '#remove_btn', function () {
+        $(this).closest('#dynamic_field_append').remove();
     });
 </script>
 @endsection
